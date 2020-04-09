447fcdffeadc293e96d4dbffca07a5b4f558ed80
Dustin McCraw <dustin.mccraw@teamsnap.com>
2020-04-08 17:26:25 -0700

    update some configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   siteMetadata: {
-    title: `Creative Portfolio`,
+    title: `The McCraws`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
